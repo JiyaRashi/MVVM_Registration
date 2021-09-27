0aeaaa27f43d59c35df922e70eac3ee32c65c2e6
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-09-27 20:45:47 +0530

    model added

---
+++ MVVMReg/MVVM_Registration/Model/Users.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVM_Registration.Model
+{
+    public class Users
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Int64 MobileNumber { get; set; }
+
+    }
+}
