2b7f5990e5a9253b300cbb43d1ef9c8327888192
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-09-30 16:37:06 +0530

    Dependency Control

---
+++ MVVMReg/MVVM_Registration/UserControls/MandatoryControl.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVM_Registration.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MandatoryControl.xaml
+    /// </summary>
+    public partial class MandatoryControl : UserControl
+    {
+        public MandatoryControl()
+        {
+            InitializeComponent();
+        }
+
+        public string RequiredSymbol
+        {
+            get => (string)GetValue(RequiredSymbolProperty);
+            set => SetValue(LabelTextProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for RequiredSymbol.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty RequiredSymbolProperty =
+            DependencyProperty.Register("RequiredSymbol", typeof(string), typeof(MandatoryControl), new PropertyMetadata("*"));
+
+
+        public string LabelText
+        {
+            get { return (string)GetValue(LabelTextProperty); }
+            set { SetValue(LabelTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for RequiredSymbol.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty LabelTextProperty =
+            DependencyProperty.Register("LabelText", typeof(string), typeof(MandatoryControl), new PropertyMetadata("Name"));
+    }
+}
--- MVVMReg/MVVM_Registration/ViewModels/UserViewodel.cs
+++ MVVMReg/MVVM_Registration/ViewModels/UserViewodel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVVM_Registration.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,12 @@ namespace MVVM_Registration.ViewModels
 {
     public class UserViewodel
     {
+        public IList<Users> UsersList { get; set; }
         public UserViewodel()
         {
+            UsersList = new List<Users>();
+            UsersList.Add( new Users() { FirstName = "Jiyavudeen", LastName = "Junaid" ,MobileNumber=8098820270});
+            UsersList.Add(new Users() { FirstName = "Rasheetha Begam", LastName = "Junaid", MobileNumber = 7358847004 });
 
         }
     }
