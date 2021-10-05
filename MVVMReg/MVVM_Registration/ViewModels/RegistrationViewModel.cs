ef7964180a2b10b156613224e09d1673c1bbfddb
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-10-05 13:16:06 +0530

    added User data View

--- MVVMReg/MVVM_Registration/ViewModels/RegistrationViewModel.cs
+++ MVVMReg/MVVM_Registration/ViewModels/RegistrationViewModel.cs
@@ -11,16 +11,27 @@ namespace MVVM_Registration.ViewModels
 {
     public class RegistrationViewModel
     {
+        private ObservableCollection<Users> _users;
         public RegistrationViewModel()
         {
-
+            Users = GetAllUsers();
         }
 
         public ObservableCollection<Users> GetAllUsers()
         {
             return new UserData().GetUserList();
         }
-
+        public ObservableCollection<Users> Users
+        {
+            get
+            {
+                return _users;
+            }
+            set
+            {
+                _users = value;
+            }
+        }
 
     }
 }
