68ae57cad6c680706c4b03a50c7280703d60b320
Jiyavudeen r <jiyavudeen.r379@outlook.com>
2021-10-07 13:34:06 +0530

    changes updated

--- MVVMReg/MVVM_Registration/Model/Users.cs
+++ MVVMReg/MVVM_Registration/Model/Users.cs
@@ -10,7 +10,31 @@ namespace MVVM_Registration.Model
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string FullName { get; set; }
+        public Language Language { get; set; }
+        public Country Country { get; set; }
+        public Int64 Pincode { get; set; }
+        public string Email { get; set; }
         public Int64 MobileNumber { get; set; }
 
+    }
+
+    public enum Language
+    {
+        Tamil,
+        English,
+        Hindi,
+        Malayalam,
+        Telugu,
+    }
+
+    public enum Country
+    {
+        India,
+        England,
+        Australia,
+        Japan,
+        WestIndies,
+        Italy,
     }
 }
--- MVVMReg/MVVM_Registration/ViewModels/RegistrationViewModel.cs
+++ MVVMReg/MVVM_Registration/ViewModels/RegistrationViewModel.cs
@@ -10,10 +10,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Collections.Generic;
 
 namespace MVVM_Registration.ViewModels
 {
-    public class RegistrationViewModel : INotifyPropertyChanged
+    public class RegistrationViewModel : INotifyPropertyChanged,IDataErrorInfo
     {
         private ICommand addCommand;
 
@@ -24,28 +25,95 @@ namespace MVVM_Registration.ViewModels
         private string _firstName;
         private string _lastName;
         private Int64 _mobileNumber;
+        private string _fullName;
+        private Language _language;
+        private Country _country;
+        private Int64 _pinCode;
+
+        private string _email;
+        private readonly Dictionary<string, List<string>> _propertyNameToErrorsDictionary;
+        private bool HasFirstName => !string.IsNullOrEmpty(FirstName);
 
         public RegistrationViewModel()
         {
             Users = new BusinessLogic().GetAllUsers();
+            _propertyNameToErrorsDictionary = new Dictionary<string, List<string>>();
         }
 
         public Int64 MobileNumber
         {
             get { return _mobileNumber; }
-            set { _mobileNumber = value; }
+            set { 
+                _mobileNumber = value;
+                OnPropertyRaised("MobileNumber");
+
+            }
         }
         public string FirstName
         {
             get { return _firstName; }
-            set { _firstName = value; }
+            set { 
+                _firstName = value;
+                if (HasFirstName)
+                {
+                }
+                OnPropertyRaised("FirstName");
+
+            }
         }
 
 
         public string LastName
         {
             get { return _lastName; }
-            set { _lastName = value; }
+            set {
+                _lastName = value;
+                OnPropertyRaised("LastName");
+
+            }
+        }
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyRaised("Email");
+
+            }
+        }
+
+        public string FullName
+        {
+            get { 
+                return _firstName + " " + _lastName; ;
+            }
+            set
+            {
+                _fullName = value;
+                OnPropertyRaised("FullName");
+
+            }
+        }
+
+
+        public Language Language
+        {
+            get { return _language; }
+            set { _language = value; }
+        }
+
+        public Country Country
+        {
+            get { return _country; }
+            set { _country = value; }
+        }
+
+        public Int64 PinCode
+        {
+            get { return _pinCode; }
+            set { _pinCode = value; }
         }
 
         public ICommand AddCommand
@@ -89,9 +157,23 @@ namespace MVVM_Registration.ViewModels
             }
         }
 
+        public string Error => throw new NotImplementedException();
+
+        public string this[string columnName] => throw new NotImplementedException();
+
         public void AddUsers()
         {
-            Users.Add(new Users() { FirstName = FirstName, LastName = LastName, MobileNumber = MobileNumber });
+            Users.Add(new Users() 
+            {
+                FirstName = FirstName, 
+                LastName = LastName, 
+                FullName=FullName,
+                Language=Language,
+                Email=Email,
+                MobileNumber = MobileNumber,
+                Country=Country,
+                Pincode=PinCode
+            });
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
